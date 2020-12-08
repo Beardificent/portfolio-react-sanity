021623ed0ee99748f361359fe6b7ba77628e6235
Beardificent <cis.magito@gmail.com>
2020-12-08 22:27:25 +0100

    started using tailwindcss to give style to my navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,42 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export default function NavBar(){
-    return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
-                        Home
-                    </NavLink>
-                    <NavLink to="/post">
-                        Post
-                    </NavLink>
-                    <NavLink to="/project">
-                        Projects
-                    </NavLink>
-                    <NavLink to="/about">
-                        About Me
-                    </NavLink>
-                </nav>
-            </div>
-        </header>
-    )
+export default function NavBar() {
+  return (
+    <header className="bg-pink-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-black-200 hover:text-white-500 text-4xl font-bold cursive tracking-widest"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-black-200 hover:text-yellow-200"
+            activeClassName="text-pink-200 bg-pink-900"
+          >
+            Post
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-black-200 hover:text-yellow-200"
+            activeClassName="text-pink-200 bg-pink-900"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-black-200 hover:text-yellow-200"
+            activeClassName="text-pink-200 bg-pink-900"
+          >
+            About Me
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
