cb14f3d2abe349a27b3569ad0caca94689586fa3
Beardificent <cis.magito@gmail.com>
2020-12-09 15:25:25 +0100

    styled the landing page and started working on the Post page, using groq to link with sanity

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,20 @@
 import React from "react";
+import image from "../firewatch-bg.jpeg";
 
-export default function Home(){
-    return <h1>Home</h1>
+export default function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt="firewatch-bg"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-scren pt-12 lg:pt-64 px-8">
+        <h1 className="text)6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          {" "}
+          Portfolio{" "}
+        </h1>
+      </section>
+    </main>
+  );
 }
